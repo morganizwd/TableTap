7a1d0eab824ae70a0bb8385550f4abfe2f030522
Ilya Lobanovskiy <morganizwd@gmail.com>
2023-12-18 19:03:12 +0300

    feat: add review model

---
+++ models/review.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose"
+
+const ReviewSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        }, 
+        restaurant: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Restaurant',
+            required: true,
+        },
+        text: String,
+        rate: Number,
+    }
+)
