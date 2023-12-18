234aede86ece627e26ef741efdf2e90e38961df2
Ilya Lobanovskiy <morganizwd@gmail.com>
2023-12-18 17:36:59 +0300

    feat: add modules

---
+++ index.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import mongoose from 'mongoose'; 
+import multer from 'multer';
+import cors from 'cors';
+
+mongoose 
+    .connect('mongodb+srv://admin:Hesus2016@cluster0.vgtv5yo.mongodb.net/TableTap')
+    .then(() => console.log('DB OK'))
+    .catch((err) => console.log('DB ERROR', err)); 
+
+const app = express();
+
+const storage = multer.diskStorage({ 
+    destination: (_, __, cb) => { 
+        cb(null, 'uploads') 
+    }, 
+    filename: (_, file, cb) => { 
+        cb(null, file.originalname) 
+    }, 
+});
+
+const upload = multer({ storage });
+
+app.use(express.json()); 
+app.use(cors());
+// app.use('/uploads', express.static('uploads')); 
+
+// //media upload pathes
+// app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+//     res.json({
+//         url: `/uploads/${req.file.originalname}`,
+//     });
+// });
+
+app.listen(4444, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+
+    console.log('Server OK');
+});
