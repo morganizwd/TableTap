cd1024fa2b46ef17ce175384af555f000d976e9d
Ilya Lobanovskiy <morganizwd@gmail.com>
2023-12-18 21:33:17 +0300

    redactor: do some with exports

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,3 @@
 export * as UserController from './UserController.js';
+export * as RestraurantController from './RestaurantController.js';
+export * as ReviewController from './ReviewController.js';
