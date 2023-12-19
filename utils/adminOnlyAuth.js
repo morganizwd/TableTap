bc41a8ae34f86ef93583856d3ddf1c24b6aca6dc
Ilya Lobanovskiy <morganizwd@gmail.com>
2023-12-19 23:13:49 +0300

    feat: add admin only auth

---
+++ utils/adminOnlyAuth.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+
+export default (req, res, next) => {
+    const token = (req.headers.authorization || '').replace(/Bearer\s?/, '');
+
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, 'secret123');
+
+            req.userId = decoded._id;
+            req.userRole = decoded.role; // Сохраните роль пользователя в запросе
+
+            // Проверка роли пользователя
+            if (!['superAdmin', 'restaurantAdmin'].includes(req.userRole)) {
+                return res.status(403).json({
+                    message: 'Access denied: insufficient role',
+                });
+            }
+
+            next();
+        } catch (e) {
+            return res.status(403).json({
+                message: 'Access denied',
+            });
+        }
+    } else {
+        return res.status(403).json({
+            message: 'Access denied',
+        });
+    }
+};
--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,4 @@
 export {default as highRolesAuth} from './highRolesAuth.js';
 export {default as allRolesAuth} from './allRolesAuth.js';
+export {default as adminOnlyAuth} from './adminOnlyAuth.js';
 export {default as handleValidationErrors} from './handleValidationErrors.js'; 
