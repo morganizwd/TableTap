abe7cc2c75502cd7b9287d092b3fe0dd83d9022c
Ilya Lobanovskiy <morganizwd@gmail.com>
2023-12-21 00:30:16 +0300

    feat: add router to app

--- client/tabletap/src/App.js
+++ client/tabletap/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Header from './Components/header';
 import Footer from './Components/footer';
 import RestaurantsPage from './Components/RestaurantsPage/RestaurantsPage';
@@ -10,21 +12,30 @@ import SuperAdminPage from './Components/SuperAdminPage';
 import CreateReservationPage from './Components/CreateReservationPage';
 import AboutPage from './Components/AboutPage';
 import ContactPage from './Components/Contacts';
+import RegistrationPage from './Components/RegistrationPage';
+import LoginPage from './Components/loginPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      {/* <RestaurantsPage/> */}
-      {/* <RestaurantProfile/> */}
-      {/* <UserProfile/> */}
-      {/* <RestaurantAdminPage/> */}
-      {/* <SuperAdminPage/> */}
-      {/* <CreateReservationPage/> */}
-      {/* <AboutPage/> */}
-      {/* <ContactPage/> */}
-      <Footer/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header/>
+            <Routes>
+              <Route path='/restaurants' element={<RestaurantsPage/>}></Route>
+              <Route path='/restaurants/profile' element={<RestaurantProfile/>}></Route>
+              <Route path='/user' element={<UserProfile/>}></Route>
+              <Route path='/restaurant-admin-page' element={<RestaurantAdminPage/>}></Route>
+              <Route path='/adminpage' element={<SuperAdminPage/>}></Route>
+              <Route path='/restaurants/restaurant/reservation' element={<CreateReservationPage/>}></Route>
+              <Route path='/about-us' element={<AboutPage/>}></Route>
+              <Route path='/contacts' element={<ContactPage/>}></Route>
+              <Route path='/registration' element={<RegistrationPage/>}/>
+              <Route path='/login' element={<LoginPage/>}/>
+            </Routes>
+        <Footer/>
+      </div>
+    </BrowserRouter>
   );
 }
 
