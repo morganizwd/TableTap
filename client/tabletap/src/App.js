e60311711b15745ee91575010d1defecdc3fe417
Ilya Lobanovskiy <morganizwd@gmail.com>
2023-12-21 00:10:19 +0300

    feat: import pages in app

--- client/tabletap/src/App.js
+++ client/tabletap/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './Components/header';
+import Footer from './Components/footer';
+import RestaurantsPage from './Components/RestaurantsPage/RestaurantsPage';
+import RestaurantProfile from './Components/RestaurantsPage/RestaurantProfile';
+import UserProfile from './Components/UserProfile';
+import RestaurantAdminPage from './Components/RestaurantAdminPage';
+import SuperAdminPage from './Components/SuperAdminPage';
+import CreateReservationPage from './Components/CreateReservationPage';
+import AboutPage from './Components/AboutPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      {/* <RestaurantsPage/> */}
+      {/* <RestaurantProfile/> */}
+      {/* <UserProfile/> */}
+      {/* <RestaurantAdminPage/> */}
+      {/* <SuperAdminPage/> */}
+      {/* <CreateReservationPage/> */}
+      <AboutPage/>
+      <Footer/>
     </div>
   );
 }
