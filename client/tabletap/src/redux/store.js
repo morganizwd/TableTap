e08e40cf4f4bc7a0e424ac2a4bb12898e5f0de9a
Ilya Lobanovskiy <morganizwd@gmail.com>
2023-12-24 15:33:54 +0300

    feat: add slice for auth

--- client/tabletap/src/redux/store.js
+++ client/tabletap/src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import { restaurantReducer } from './slices/restaurants';
 import { reviewReducer } from './slices/reviews';
 import { reseravationReducer } from './slices/reservations';
+import { authReducer } from './slices/auth';
 
 const store = configureStore({
     reducer: {
         restaurants: restaurantReducer,
         reviews: reviewReducer,
         reservations: reseravationReducer,
+        auth: authReducer,
     }
 });
 
