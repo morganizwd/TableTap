a2892d088fd9b777383250b4143f0f9456935abd
Ilya Lobanovskiy <morganizwd@gmail.com>
2023-12-25 13:15:59 +0300

    reafctor: change user profile for working with new fetch from auth slice

--- client/tabletap/src/Components/UserProfile.jsx
+++ client/tabletap/src/Components/UserProfile.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   Container,
   Box,
@@ -9,17 +9,34 @@ import {
   List,
   ListItem,
   ListItemText,
-  Divider,
+  Divider, 
 } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUserById } from '../redux/slices/auth';
+import { fetchReviewsByUser } from '../redux/slices/reviews';
+import { useParams } from 'react-router-dom';
 
-function UserProfile() {
-  // Демонстрационные данные пользователя
-  const user = {
-    fullName: "Alexander Trubov",
-    email: "test5@test.com",
-    role: "restaurantAdmin",
-    avatarUrl: "https://i.pinimg.com/564x/84/7f/e9/847fe98af13d049a78bf28738ea6e166.jpg",
-  };
+const UserProfile = () => {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const user = useSelector((state) => state.auth.userData);
+  const { reviews } = useSelector(state => state.reviews);
+  
+  useEffect(() => {
+    if (id) {
+        dispatch(fetchUserById(id));
+    }
+  }, [dispatch, id]);
+
+  useEffect(() => {
+    if (user) {
+        dispatch(fetchReviewsByUser(id));
+    }
+  }, [dispatch, user, id]);
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <Container>
@@ -28,7 +45,7 @@ function UserProfile() {
           <Grid item xs={12} sm={4} md={3} lg={2}>
             <Avatar
               alt={user.fullName}
-              src={user.avatarUrl}
+              src={user.avatarUrl || "https://via.placeholder.com/150"}
               sx={{ width: 150, height: 150 }}
             />
           </Grid>
@@ -45,13 +62,41 @@ function UserProfile() {
 
         <Divider sx={{ my: 2 }} />
 
-        {/* Дополнительная информация о пользователе */}
+        {/* <Typography variant="h5" gutterBottom sx={{ mt: 4 }}>
+            Отзывы
+        </Typography>
+        <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+          {reviews.map((review, index) => (
+                <React.Fragment key={review._id}>
+                    <ListItem alignItems="flex-start">
+                        <ListItemText
+                        primary={`Отзыв от ${new Date(review.createdAt).toLocaleDateString()}`}
+                        secondary={
+                            <>
+                            <Typography
+                                sx={{ display: 'inline' }}
+                                component="span"
+                                variant="body2"
+                                color="text.primary"
+                            >
+                                Рейтинг: {review.rating}
+                            </Typography>
+                            {" — " + review.text}
+                            </>
+                        }
+                        />
+                    </ListItem>
+                    {index < reviews.length - 1 && <Divider variant="inset" component="li" />}
+                </React.Fragment>
+            ))}
+        </List> */}
+
         <List>
           <ListItem>
-            <ListItemText primary="Дата создания аккаунта" secondary="18 декабря 2023" />
+            <ListItemText primary="Дата создания аккаунта" secondary={new Date(user.createdAt).toLocaleDateString()} />
           </ListItem>
           <ListItem>
-            <ListItemText primary="Последнее обновление" secondary="18 декабря 2023" />
+            <ListItemText primary="Последнее обновление" secondary={new Date(user.updatedAt).toLocaleDateString()} />
           </ListItem>
         </List>
       </Paper>
